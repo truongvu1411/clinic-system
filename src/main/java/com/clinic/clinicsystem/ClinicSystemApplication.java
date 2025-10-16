59efb51cc3d4e14608f9a67858f678590fc8af65
Phạm Trường Vũ <vusnake@gmail.com>
2025-10-16 15:40:16 +0700

    Initial commit - Clinic Management System

---
+++ src/main/java/com/clinic/clinicsystem/ClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.clinicsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSystemApplication.class, args);
+	}
+
+}
